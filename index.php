6f76f1bce0ee5691cc6b63889d863e5c27f2f3f8
winxtxr <107090196+winxtxr@users.noreply.github.com>
2023-06-21 11:30:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<link rel = "stylesheet" href = "style.css"/>
+
+<?php 
+$name = "<center><h2>วินสุดหล่อนำเสนอลิงกัง &nbsp;&nbsp;&nbsp;</center><br/> </h2>";  
+$age = 18;
+echo "<h1> ยินดีต้อนรับไอสัส </h1>";
+echo "$name";
+//$year = 2566;
+//$byear = $year-$age;
+ // echo "พ.ศ.เกิด $byear <br/> ";
+  echo "<center><img src='images/3.png' height ='200px'/> </center>";
+?>
+
+<p class = "f1">
+กูลืมรหัสผ่าน 😭
+</p>
---
+++ login.php
@@ -0,0 +1,10 @@
+<link rel = "stylesheet" href = "style.css"/>
+
+<h2id = "f1">  Login   </h2> <br/> <br/> 
+<form method="post" action="checklogin.php" enctype="multipart/form-data">
+    Username :
+    <input name="username" type="text" /> <br/> <br/>
+    Password :
+    <input name="password" type="password" /> <br/> <br/>
+    <input type="submit" value=" LOGIN " />
+</form>
